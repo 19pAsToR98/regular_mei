f35eedd4c6f95acd8e5c9b4d0054f9406bf7c3d9
[dyad] <portalmei360@gmail.com>
2025-12-21 08:03:08 -0300

    [dyad] Alterando o ícone do assistente virtual para um GIF - wrote 1 file(s)

--- components/VirtualAssistantButton.tsx
+++ components/VirtualAssistantButton.tsx
@@ -12,7 +12,7 @@ const VirtualAssistantButton: React.FC<VirtualAssistantButtonProps> = ({ isOpen,
       className={`
         fixed bottom-6 right-6 z-40 
         w-16 h-16 rounded-full 
-        bg-primary text-white
+        bg-primary
         shadow-xl hover:scale-110 
         transition-all duration-500 ease-in-out
         flex items-center justify-center 
@@ -21,7 +21,12 @@ const VirtualAssistantButton: React.FC<VirtualAssistantButtonProps> = ({ isOpen,
       aria-label="Abrir Assistente Virtual"
       title="Assistente Virtual"
     >
-      <span className="material-icons text-3xl">smart_toy</span>
+      {/* Usando o GIF em vez do Material Icon */}
+      <img 
+        src="/clippy-white-10.gif" 
+        alt="Assistente Dyad" 
+        className="w-10 h-10 object-contain"
+      />
       
       {/* Pequeno balão de fala para simular interação */}
       <div className="absolute top-0 left-0 w-4 h-4 bg-green-400 rounded-full border-2 border-white dark:border-slate-900 animate-pulse"></div>
